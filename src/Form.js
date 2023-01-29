150f14da2ca40f6d53897dc27172915366fc7062
Vepa Hocanazarov <vefa00055@gmail.com>
2023-01-29 16:00:45 +0300

    JSX içinde binding yapma

--- src/Form.js
+++ src/Form.js
@@ -3,21 +3,21 @@ import React, { Component } from 'react'
 
  class Form extends Component {
 
-    constructor(props)
+    constructor(props)  // Constructor ile binding yapma
     {
         super(props);
         // this.addButton = this.addButton.bind(this); // ==>> Aşagida Arrow function ile addnews'e binding yapdıgımız için buna gerek yok...
     };
 
-    addButton = () =>
+    addButton() 
     {
-        this.props.addnews();
+        this.props.addnews();   // Arrow function ile binding yapma
         console.log("hi");
     }
   render() {
     return (
       <div>
-        <button onClick={this.addButton}>Ekle</button>
+        <button onClick={() => this.addButton () }>Ekle</button> 
       </div>
     )
   }
