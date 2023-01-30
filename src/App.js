4190288c3ff33edbd5fa201219cf3e9a0213efec
Vepa Hocanazarov <vefa00055@gmail.com>
2023-01-30 12:02:23 +0300

    Counter componenti oluşturuldu ve import edildi

--- src/App.js
+++ src/App.js
@@ -4,80 +4,83 @@ import { Component } from 'react';
 import Aygul from './Aygul';
 import Arakatmann from './Arakatmann';
 import './Deneme.css';
+import Counter from './Counter';
 
-const name = "Vepa";
-    const lastname = "Hocanazarov"
+// const name = "Vepa";
+//     const lastname = "Hocanazarov"
 
-    const dizim = [{
-      id: 1,
-      title: "Hiper",
-      description: "React"
-    },
-    {
-      id: 2,
-      title: "Süper",
-      description: "React Egitimi"
-    },
-    {
-      id: 3,
-      title: "Hiper Süper",
-      description: "React'in Kendisi"
-    },
-    ]
+//     const dizim = [{
+//       id: 1,
+//       title: "Hiper",
+//       description: "React"
+//     },
+//     {
+//       id: 2,
+//       title: "Süper",
+//       description: "React Egitimi"
+//     },
+//     {
+//       id: 3,
+//       title: "Hiper Süper",
+//       description: "React'in Kendisi"
+//     },
+//     ]
 
 
 class App extends Component {
 
 
-  state = {
-    name:"Aygül" 
-     // State:Uygulama içinde degeri degişebilen degerleri tutan Javascript objesidir.
-  }
- 
+  // state = {
+  //   name:"Aygül" 
+  //    // State:Uygulama içinde degeri degişebilen degerleri tutan Javascript objesidir.
+  // }
 
-  addnews() {
 
-    console.log("Ekledim");
+  // addnews() {
 
-    dizim.push({
-      id:4,
-      title:"Muhteşem",
-      description:"Muhteşem ötesi"
-    });
+  //   console.log("Ekledim");
 
-    console.log(dizim)
-  }
+  //   dizim.push({
+  //     id:4,
+  //     title:"Muhteşem",
+  //     description:"Muhteşem ötesi"
+  //   });
 
-  btnpratik()
-  {
-    console.log("btn pratik tiklandi");
-  }
-  
-  changeName ()
-  {
-    console.log(this.state)
-    this.setState(
-      {
-        name : "Vepa"
-      }
-    )
-    
-  }
+  //   console.log(dizim)
+  // }
+
+  // btnpratik()
+  // {
+  //   console.log("btn pratik tiklandi");
+  // }
+
+  // changeName ()
+  // {
+  //   console.log(this.state)
+  //   this.setState(
+  //     {
+  //       name : "Vepa"
+  //     }
+  //   )
+
+  // }
 
   render() {
 
-    
+
 
     return (
 
       <div className="container">
 
-        <h1> {this.state.name} </h1>
+        <Counter/>
+
+        {/* <h1> {this.state.name} </h1>
 
         <button onClick={this.changeName.bind(this)}>Degiştir</button>
 
         <Aygul isim={name} soyad={lastname} btnpratik = {this.btnpratik} />
-        {/* <Arakatmann dizim={dizim} addnews={this.addnews} /> */}
+        <Arakatmann dizim={dizim} addnews={this.addnews} /> */}
 
       </div>
 
