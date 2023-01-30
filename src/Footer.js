5976df13de4d2f1a60db83cfe042de9a707e6da0
Vepa Hocanazarov <vefa00055@gmail.com>
2023-01-30 14:52:22 +0300

    StateLess component

--- src/Footer.js
+++ src/Footer.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react'
 
- class Footer extends Component {
-  render() {
-    return (
+ const Footer = () =>
+
       <div>
 
        <br /><br /><br /><br /><br /><br /> Canboz  
         
-      </div>  // bu bir StateFull componentdir cünkü ben buraya state ekeleyebiliyorum
-    )
-  }
-}
+      </div>  // bu ise StateLess componentdir render edilecekelri class component olarak tanımlamamız lazım onun dışındakileri const component olarak tanımlarız nedeni ise 
+               //Footer gibi degişmeyen alanlarda const componenti kullanırız clean code prensibine bunu gerektitir
+
 
 export default Footer;
